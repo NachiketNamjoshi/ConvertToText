391a384ce9a968f8482e50213799cfd145956767
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2017-02-03 22:51:23 +0530

    pdfConvertor: Added proper line conversion

--- ConvertToText/pdfConvertor.py
+++ ConvertToText/pdfConvertor.py
@@ -2,7 +2,7 @@ import os
 from os import chdir, getcwd, listdir, path
 import PyPDF2 as pyPdf
 from time import strftime
-
+import re
 def check_path(prompt):
     abs_path = input(prompt)
     while path.exists(abs_path) != True:
@@ -17,8 +17,19 @@ def extract_text():
     pdf = pyPdf.PdfFileReader(open(path,"rb"))
     for i in range(0, pdf.getNumPages()):
         content += pdf.getPage(i).extractText() + "\n"
+    content = ParaToSentence(content)
     print (strftime("%H:%M:%S"), " pdf -> txt ")
     f = open(name,'wb')
     f.write(content.encode("UTF-8"))
     f.close
     return name
+
+def ParaToSentence(content):
+    ConvertedText = ""
+    sentences = re.split(r'((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s)', content)
+    for line in sentences:
+        if re.match(r'^\s*$',line): ##To skip blank lines from op.
+            continue
+        ConvertedText += line
+    return ConvertedText
+
--- examples/pdf_to_text.py
+++ examples/pdf_to_text.py
@@ -3,7 +3,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-import ConTxt.pdfConvertor as pyPDF
+import ConvertToText.pdfConvertor as pyPDF
 
 converted_path = pyPDF.extract_text()
 print ("File Saved to: "+converted_path)
