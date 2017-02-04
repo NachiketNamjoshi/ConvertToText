55a990474fafe1e68a6e289ca34fa16ba33888a0
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2017-02-04 10:55:00 +0530

    pdfConvertor: Use Class now

--- ConvertToText/pdfConvertor.py
+++ ConvertToText/pdfConvertor.py
@@ -1,35 +1,46 @@
 import os
+import sys
 from os import chdir, getcwd, listdir, path
 import PyPDF2 as pyPdf
 from time import strftime
 import re
-def check_path(prompt):
-    abs_path = input(prompt)
-    while path.exists(abs_path) != True:
-        print ("\nThe Specified path does not exist.\n")
-        abs_path = input(prompt)
-    return abs_path
 
-def extract_text():
-    path = check_path("Absolute Path to folder containing all pdfs: ")
-    name = path.replace(".pdf",".txt")
-    content = ""
-    pdf = pyPdf.PdfFileReader(open(path,"rb"))
-    for i in range(0, pdf.getNumPages()):
-        content += pdf.getPage(i).extractText() + "\n"
-    content = ParaToSentence(content)
-    print (strftime("%H:%M:%S"), " pdf -> txt ")
-    f = open(name,'wb')
-    f.write(content.encode("UTF-8"))
-    f.close
-    return name
+class pdfConvertor(object):
 
-def ParaToSentence(content):
-    ConvertedText = ""
-    sentences = re.split(r'((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s)', content)
-    for line in sentences:
-        if re.match(r'^\s*$',line): ##To skip blank lines from op.
-            continue
-        ConvertedText += line
-    return ConvertedText
+    def __init__(self,inputFile):
+        inputFile = self.check_path(inputFile)
+        if inputFile != None:
+             self.extract_text(inputFile)
+        else:
+            sys.exit(1)
+
+
+    def check_path(self, abs_path):
+        if path.exists(abs_path) != True:
+            print ("\nThe Specified path does not exist.\n")
+            return None
+        return abs_path
+
+    def extract_text(self, path):
+        content = ""
+        name = path.replace(".pdf",".txt")
+        pdf = pyPdf.PdfFileReader(open(path,"rb"))
+        for i in range(0, pdf.getNumPages()):
+            content += pdf.getPage(i).extractText() + "\n"
+        content = self.ParaToSentence(content)
+        print (strftime("%H:%M:%S"), " pdf -> txt ")
+        f = open(name,'wb')
+        f.write(content.encode("UTF-8"))
+        f.close
+        print("File Saved To: "+name)
+        return
+
+    def ParaToSentence(self, content):
+        ConvertedText = ""
+        sentences = re.split(r'((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s)', content)
+        for line in sentences:
+            if re.match(r'^\s*$',line): ##To skip blank lines from op.
+                continue
+            ConvertedText += line
+        return ConvertedText
 
--- examples/pdf_to_text.py
+++ examples/pdf_to_text.py
@@ -3,7 +3,6 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-import ConvertToText.pdfConvertor as pyPDF
+import ConvertToText.pdfConvertor as PyPDF
 
-converted_path = pyPDF.extract_text()
-print ("File Saved to: "+converted_path)
+PyPDF.pdfConvertor(input("Absolute Path To File: "))
